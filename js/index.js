9a934e0c094f2de145688655a53ecf477856f5bd
Matias Delgado <matiasdelgado.web@outlook.com>
2023-08-01 23:17:51 -0300

    CaC TP1 ID Presentacion

---
+++ js/index.js
@@ -0,0 +1,34 @@
+function btnInfo() {
+  var mostrar = document.getElementById("info");
+  if (mostrar.style.display === "none") {
+    mostrar.style.display = "block";
+  } else {
+    mostrar.style.display = "none";
+  }
+}
+
+function btnForm() {
+  var mostrar = document.getElementById("formacion");
+  if (mostrar.style.display === "none") {
+    mostrar.style.display = "block";
+  } else {
+    mostrar.style.display = "none";
+  }
+}
+
+function btnExp() {
+  var mostrar = document.getElementById("experiencia");
+  if (mostrar.style.display === "none") {
+    mostrar.style.display = "block";
+  } else {
+    mostrar.style.display = "none";
+  }
+}
+function btnPor() {
+  var mostrar = document.getElementById("porfolio");
+  if (mostrar.style.display === "none") {
+    mostrar.style.display = "block";
+  } else {
+    mostrar.style.display = "none";
+  }
+}
